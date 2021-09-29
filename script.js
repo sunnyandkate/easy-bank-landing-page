1137e0f606f05dbf48c95a9aed11bc755da096ce
Kathrin Markworth <sunnyandkate@Kates-Air.fritz.box>
2021-09-29 11:47:56 +0100

    new commit

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 const cont = document.querySelector('.main-container');
 const mobileMenu = document.querySelector('.mobile-menu');
-
 const navMenu = document.querySelector('.nav-menu');
 const menuContent = document.querySelector('.menuContent');
-
 const overlayContainer = document.querySelector('.overlay-container');
 
 //toggle mobile menu
-mobileMenu.addEventListener('click', function(){
-	
-	
+mobileMenu.addEventListener('click', function(){	
 	navMenu.classList.toggle('menuContent');
 	mobileMenu.classList.toggle('open');
 	overlayContainer.classList.toggle('overlay');
